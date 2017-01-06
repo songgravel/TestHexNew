a61d066b4bfbd25f6f3896ee7f958fb683227c76
song <songgravel@163.com>
2017-01-06 17:05:56 +0800

    init

---
+++ Assets/Test/Scripts/HexCell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CobraGame
+{
+    public class HexCell : MonoBehaviour
+    {
+        public HexCoordinates coordinates;
+        public Color color;
+    }
+}
---
+++ Assets/Test/Scripts/HexMetrics.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace CobraGame
+{
+    public static class HexMetrics
+    {
+        private const float SQRT32 = 0.86602540378443864676372317075294f;   //sqrt(3)/2
+
+        public const float outerRadius = 10f;
+
+        public const float innerRadius = outerRadius * SQRT32;
+
+        public static Vector3[] corners = {
+            new Vector3(0f, 0f, outerRadius),
+            new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+            new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+            new Vector3(0f, 0f, -outerRadius),
+            new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+            new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+            new Vector3(0f, 0f, outerRadius)
+        };
+    }
+}
